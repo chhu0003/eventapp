4d593a39f68864c9099814937613af4fafeaff38
chhu0003 <chhu0003@algonquinlive.com>
2020-04-20 10:50:11 -0400

    Add dummy data and mapstate

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,18 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create an Event, {{ user.name }}</h1>
+    <p>This event is created by {{ user.name }}</p>
+    <li v-for="item in categories" :key="item.id">
+      {{ item }}
+    </li>
+  </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  computed: {
+    ...mapState(['categories', 'user'])
+  }
+}
+</script>
