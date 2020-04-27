31f36135e6d70a950d07f39274fcc883bf72117d
chhu0003 <chhu0003@algonquinlive.com>
2020-04-27 13:52:19 -0400

    Added validators

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
     <label v-if="label">{{ label }}</label>
-    <input :value="value" v-bind="$attrs" @input="updateValue" />
+    <input
+      :value="value"
+      @input="updateValue"
+      v-bind="$attrs"
+      v-on="listeners"
+    />
   </div>
 </template>
 
@@ -9,12 +14,22 @@
 export default {
   inheritAttrs: false,
   props: {
-    value: [String, Number],
-    label: String
+    label: {
+      type: String,
+      default: ''
+    },
+    value: [String, Number]
+  },
+  computed: {
+    listeners() {
+      return {
+        ...this.$listeners,
+        input: this.updateValue
+      }
+    }
   },
   methods: {
     updateValue(event) {
-      // <-- method triggered by input event
       this.$emit('input', event.target.value)
     }
   }
--- src/components/BaseSelect.vue
+++ src/components/BaseSelect.vue
@@ -1,18 +1,23 @@
 <template>
   <div>
     <label v-if="label">{{ label }}</label>
-    <select :value="value" @change="updateValue" v-bind="$attrs">
+    <select
+      :value="value"
+      @change="updateValue"
+      v-bind="$attrs"
+      v-on="listeners"
+    >
       <option
         v-for="option in options"
-        :selected="option === value"
         :value="option"
-        :key="option"
+        :key="option.id"
+        :selected="option === value"
+        >{{ option }}</option
       >
-        {{ option }}
-      </option>
     </select>
   </div>
 </template>
+
 <script>
 export default {
   inheritAttrs: false,
@@ -23,6 +28,14 @@ export default {
     },
     value: [String, Number],
     label: String
+  },
+  computed: {
+    listeners() {
+      return {
+        ...this.$listeners,
+        input: this.updateValue
+      }
+    }
   },
   methods: {
     updateValue(event) {
